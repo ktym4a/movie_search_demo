3ee5af8b7c111b57db0831ffb420525874a0c0a1
“ktym4a” <s19585574@gmail.com>
2020-11-16 10:21:17 +0700

    change postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,12 @@ module.exports = {
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer'),
     require('@fullhuman/postcss-purgecss')({
-      content: ['./src/**/*.js', './public/index.html'],
+      content: [
+        './src/**/*.js',
+        './src/**/*.tsx',
+        './src/**/*.ts',
+        './public/index.html',
+      ],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
